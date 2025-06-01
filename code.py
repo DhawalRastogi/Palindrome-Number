3bdb506d34423e5aebb2641a114e6b49f6be6cbf
Dhawal Rastogi <70315215+DhawalRastogi@users.noreply.github.com>
2025-06-01 19:03:09 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,21 +1,25 @@
+def isPalindrome(x):
+    if x==0:
+        return True
+
+    elif x>0:
+        length = 0
+        check = 1
+        while check>0:
+            check = int(x/(10**length))
+            length += 1
+        length -= 1
+
+        for i in range(int((length+1)/2)):
+            if int((x%10**(i+1) - x%10**i)/10**i) != x//10**(length-i-1) - 10*(x//10**(length-i)):
+                return False
+        return True
+
+    else:
+        return False
+
 print("Check whether your integer is a palindrome")
-x = int(input("Enter an integer : "))
-
-if x==0:
-  return True
-  
-elif x>0:
-  length = 0
-  check = 1
-  while check>0:
-    check = int(x/(10**length))
-    length += 1
-  length -= 1
-
-  for i in range(int((length+1)/2)):
-    if int((x%10**(i+1) - x%10**i)/10**i) != x//10**(length-i-1) - 10*(x//10**(length-i)):
-      return False
-  return True
-
+if isPalindrome(int(input("Enter an integer : "))):
+    print("Your number is a palindrome")
 else:
-  return False
+    print("Your number is NOT a palindrome")
